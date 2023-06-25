9ac7d6a77668e9a696df4c91aa45bb5eaf941c9d
Vivek <365anu65@gmail.com>
2023-06-25 12:44:08 +0530

    Completing footer designing

--- components/Footerrights.tsx
+++ components/Footerrights.tsx
@@ -1,11 +1,42 @@
-function Footer() {
+import React from "react";
+
+const Footer = () => {
   return (
-    <footer className="bg-gray-200 py-4 text-center">
-      <p className="text-gray-600 text-sm">
-        All rights reserved &copy; {new Date().getFullYear()} Collab@IITH
-      </p>
+    <footer className="bg-gray-900 text-white py-4">
+      <div className="container mx-auto px-4 flex flex-col lg:flex-row items-center justify-between h-full">
+        <div>
+          <h3 className="text-2xl font-bold text-white mb-2">Contact Us</h3>
+          <div className="flex flex-wrap mt-2">
+            <div className="mr-8 mb-4">
+              <p className="font-semibold text-lg text-white mb-2">
+                General Inquiries
+              </p>
+              <a
+                href="mailto:info@yourcompany.com"
+                className="text-blue-500 hover:text-blue-300"
+              >
+                info@yourcompany.com
+              </a>
+            </div>
+            <div>
+              <p className="font-semibold text-lg text-white mb-2">Support</p>
+              <a
+                href="mailto:support@yourcompany.com"
+                className="text-blue-500 hover:text-blue-300"
+              >
+                support@yourcompany.com
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="mt-4 text-center">
+          <p className="text-sm lg:text-base text-white">
+            &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+          </p>
+        </div>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;

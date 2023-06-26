413e0c29a1c80735ce08db009df8dbb1cfac4b0b
Vivek <365anu65@gmail.com>
2023-06-26 17:30:49 +0530

    vercel analytics added.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import Navboard from "../components/Navboard";
 import Footer from "../components/Footerrights";
 import Head from "next/head";
 import "../styles/scrollbar.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -19,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </SessionProvider>
       <Footer />
+      <Analytics />
     </>
   );
 }
